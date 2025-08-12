0f3e7c0d347e8be9870cee28ad63776e552681fd
gorasiyariddhi4@gmail.com <gorasiyariddhi4@gmail.com>
2025-08-12 14:49:44 +0530

    demo pyhon file

--- demo.py
+++ demo.py
@@ -1,6 +1,3 @@
-Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
->>> print(ridhhi gorasiya)
-SyntaxError: invalid syntax. Perhaps you forgot a comma?
->>> print('riddhi gorasiya')
-riddhi gorasiya
+print("ridhhi gorasiya")
+
+
